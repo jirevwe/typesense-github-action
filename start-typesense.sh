a8136ed2ade7cdc5fc199c604769ea4625698fcf
Raymond Tukpe <rtukpe@gmail.com>
2022-08-16 16:24:21 +0100

    chore: fix workflow files

--- start-typesense.sh
+++ start-typesense.sh
@@ -7,17 +7,9 @@ TYPESENSE_API_KEY=$4
 
 if [ -z "$TYPESENSE_VERSION" ]; then
   echo "Missing Typesense version in the [typesense-version] input. Received value: $TYPESENSE_VERSION"
-  echo "Falling back to Typesense version [0.23.1]"
-  TYPESENSE_VERSION='0.23.1'
+  echo "Falling back to Typesense version [latest]"
+  TYPESENSE_VERSION='latest'
 fi
 
 echo "Starting single-node Typesense instance"
-# mkdir /tmp/typesense-data
-docker run -d \
-  -p $TYPESENSE_PORT:8108 \
-  # -v/tmp/typesense-data:/data \
-  --name $TYPESENSE_CONTAINER_NAME \
-  typesense/typesense:$TYPESENSE_VERSION \
-  # --data-dir /data \
-  --api-key=$TYPESENSE_API_KEY \
-  --enable-cors
+docker run -d -p $TYPESENSE_PORT:8108 --name $TYPESENSE_CONTAINER_NAME typesense/typesense:$TYPESENSE_VERSION --api-key=$TYPESENSE_API_KEY --data-dir /data --enable-cors
